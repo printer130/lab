03240ce10a112cf1372a1c5a670be4644ee954c1
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-11 19:07:19 -0400

    Add register schema form register form 🦕

---
+++ helpers/zod/RegisterSchema.js
@@ -0,0 +1,32 @@
+import { z } from 'zod'
+
+export const registerSchema = z.object({
+  ci: z.number({
+    invalid_type_error: 'Numero inválido',
+    required_error: 'Debe ser un numero',
+    description: 'Debe ser un numero'
+  }).int({ message: 'Campo obligatorio.' }),
+  fullName: z.string().min(3, 'Nombre completo requerido'),
+  birth: z.date({
+    invalid_type_error: 'Tipo de fecha inválido',
+    required_error: 'Fecha Obligatoria',
+    description: 'Fecha de nacimiento'
+  }),
+  phone: z.number({
+    invalid_type_error: 'Numero inválido',
+    required_error: 'Debe ser un numero',
+    description: 'Debe ser un numero'
+  }).int(),
+  nit: z.number({
+    description: 'Se espera un número',
+    invalid_type_error: 'Numero inválido',
+    required_error: 'Campo requerido'
+  }).int({ message: 'Numero debe ser entero' }),
+  socialReason: z.string({
+    invalid_type_error: 'Campo inválido',
+    required_error: 'Campo obligatorio',
+    description: 'Campo obligatorio'
+  }).min(3, {
+    message: 'Minimo 3 caracteres'
+  })
+})
