fbcd21419babcb5b84d4bddb69e048f42cd70ada
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-10 23:43:07 -0400

    first commit 🙌

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
+import Context from 'hooks/useUserContext'
+import { Layout } from 'container/Layout'
+import { Nav } from 'components'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+    <>
+      <Context.UserProvider>
+        <Nav />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Context.UserProvider>
+    </>
+  )
 }
 
 export default MyApp
