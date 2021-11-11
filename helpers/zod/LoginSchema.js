0b2dbf648ac4b6582b2e47e31abff42d2e12967d
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-10 23:46:58 -0400

    schema validation with zod

---
+++ helpers/zod/LoginSchema.js
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const LoginSchema = z.object({
+  email: z.string({
+    required_error: 'El nombre es requerido.',
+    invalid_type_error: 'Email no es correcto'
+  }).email({ message: 'Debe ser un email válido' }),
+  password: z.string().min(6, { message: 'Contraseña corta' }),
+  laboratory: z.string()
+})
