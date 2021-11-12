4d2993628a49e6faf333009cb51f796b2aaf0dfb
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-12 14:41:40 -0400

    Add getDocByCI function 🦕

--- db/firebase/firestore.js
+++ db/firebase/firestore.js
@@ -1,17 +1,27 @@
-import { getFirestore, addDoc, collection, doc, add, setDoc } from 'firebase/firestore'
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc
+} from 'firebase/firestore'
 
 const db = getFirestore()
-
-// const ordersRef = collection(db, 'orders', `${ci}`)
-// export async function addNewOrder (data) {
-//   const { ci } = data
-
-//   return await addDoc(collection(db, `${ci}`), data)
-// }
 
 export async function addNewOrder (data) {
   const { ci, creationDate } = data
   const docRefByCI = await doc(db, `${ci}`, `${creationDate}`)
 
   return await setDoc(docRefByCI, data, { merge: true })
+}
+
+export async function getDocByCI ({ ci }) {
+  const docRef = await doc(db, ci, '1636673609.359')
+  const docSnap = await getDoc(docRef)
+
+  if (docSnap.exists()) {
+    console.log('Document data:', docSnap.data())
+    return docSnap.data()
+  }
+
+  return 'No hay data'
 }
