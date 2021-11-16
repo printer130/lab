c2e5b5991ee4be1e2de4eb190b315e010a63780b
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-15 20:41:24 -0400

    Add dicctionary keys for orderList component 🦕

---
+++ utils/orderDicc.js
@@ -0,0 +1,231 @@
+
+export const ELECTROLITOS = [
+  'Hemograma',
+  'VES',
+  'Reticulocitos',
+  'Grupo y Rh',
+  'Gota Gruesa',
+  'Resistencia Osmotica',
+  'COOMBS Directo',
+  'COOMBS Indirecto',
+  'Fe Serico',
+  'TIBC',
+  'Ferritina',
+  'Transferrina',
+  'Inv. T. Cruzi',
+  'Crioglobulina',
+  'Plasma Rico en Plaq.',
+  'Mircometodo Chagas',
+  'HLB'
+]
+export const ELECTROLITOS2 = [
+  'TP-INR',
+  'APTT',
+  'T. Trombina',
+  'T. Coagulacion',
+  'T. Sangria',
+  'Fibrinogeno',
+  'Dos. Factor Intriseco',
+  'Dos. Factor Extrinseco',
+  'Factor XIII',
+  'Inhibidor Lupidico',
+  'Dimeros D',
+  'Anti Factor Xa',
+  'Prueba de Lazo'
+]
+
+export const ELECTROLITOS3 = [
+  'Glicemia',
+  'Glicemia Post Estimulo',
+  'Tolerancia a la Glucosa',
+  'Hb Glocosilada A1',
+  'Hb Glocosilada A1C',
+  'Urea/BUN',
+  'Creatinina',
+  'Acido Urico',
+  'Colesterol',
+  'Colesterol HDL',
+  'Colesterol LDL',
+  'Trigliceridos',
+  'Lipidos Totales',
+  'VLDL',
+  'CPK',
+  'CPK MB',
+  'CK MM',
+  'Lact. Desh Hidrogenasa',
+  'Troponina I',
+  'Amilasa',
+  'Lipasa',
+  'Amonio',
+  'Lactato',
+  'Fenilalanina',
+  'Homocisteina',
+  'Vitamina B12',
+  'Acido Folico',
+  'Peptido B Natriureico',
+  'Procalcitonina',
+  'Serotonina',
+  'Dopamina',
+  'Bilirubinas D, T, I.',
+  'GOT',
+  'GPT',
+  'GGT',
+  'Fosfatasa Alcalina',
+  'Fosfatasa Acida',
+  'Fosfatasa Acida Prostatica',
+  'Proteinas Totales',
+  'Albumina',
+  'Globulina',
+  'Electroforesis de Proteinas'
+]
+
+export const ELECTROLITOS4 = [
+  'General de Orina',
+  'Depuracion de Cratinina',
+  'Proteinuria de 24 hrs.',
+  'Na en Orina',
+  'K en Orina',
+  'Ca en Orina',
+  'Ci en Orina',
+  'Catecolaminas de 24 hrs.',
+  'Microalbuminuria',
+  'Proteinuria Matinal',
+  'Creatinuria Matinal',
+  'Chlamydia en Orina',
+  'Amilasa en Orina',
+  'Proteina de Bence-Jonce',
+  'Cortisol en Orina',
+  'P en Orina',
+  'Mg en Orina',
+  'Mc. Urico en Orina',
+  'Creatinuria de 24 hrs.',
+  'pH de 24 hrs.',
+  'Urea en Orina',
+  'Li en Orina',
+  'B2 Microalb.',
+  'Test Indicam',
+  'Vitamina C',
+  'Epinefrina',
+  'Norepinefriuna',
+  'Vitamina D'
+]
+
+export const ELECTROLITOS5 = [
+  'Sodio Na',
+  'Potasio K',
+  'Cloro Cl',
+  'Calcio Ca',
+  'Calcio Ionico Ca++',
+  'Magnesio Mg',
+  'Fosforo P',
+  'Litio Li'
+
+]
+
+export const CROPOLOGIA = [
+  'Coproparasitologico x1',
+  'Coproparasitologico x2',
+  'Coproparasitologico x3',
+  'Parasitologico Concentrado',
+  'Oxiuros',
+  'Lienteria',
+  'Moco Fecal',
+  'Calprotecnina'
+]
+
+export const MICROBIOLOGIA = [
+  'Urocultivo x1',
+  'Coprocultivo',
+  'Cultivo de Secrecion',
+  'Hemocuilitivo x1',
+  'Hemocuilitivo x2',
+  'Hemocuilitivo x3',
+  'Cultivo de Hongos',
+  'BK Simple',
+  'BK Seriado',
+  'Estudio Directo',
+  'Tincion de Gram',
+  'Tincion de Zhiel Neelsen',
+  'Cultivo de Esputo',
+  'Cultivo de Sec. Vaginal',
+  'Espermocultivo',
+  'Tinta China',
+  'Cultivo Sec. Faringea o Traqueal'
+]
+export const EMBARAZO = [
+  'BHCG Cualitativa',
+  'BHCG Cuantitativa',
+  'PAPP-A',
+  'HCG Beta Libre'
+]
+export const LIQUIDOS_BIOLOGICOS = [
+  'Citoqui. LCR',
+  'Citoqui. Liq. Pleural',
+  'Citoqui. Liq. Ascitico',
+  'Citoqui. Liq. Sinovial',
+  'Citoqui. Liq. Pericardio',
+  'Citoqui. Liq. Peritoneal',
+  'ADA',
+  'Amonio LCR',
+  'Espermograma'
+]
+
+export const MARCADORES_TUMORALES = [
+  'CEA',
+  'AFP',
+  'CA - 19.9',
+  'CA - 125',
+  'CA - 15.3',
+  'PSA Total',
+  'PSA Libre',
+  'Beta 2 MircoAlb',
+  'BHCG Cuantitavivo'
+]
+
+export const SEROLOGIA = [
+  'PCR Cuantitativo',
+  'ASTO Cuantitativo',
+  'RF Cuantitativo',
+  'RPR',
+  'Reaccion Widal',
+  'Mononucleosis',
+  'Brucellosis'
+]
+
+export const ENSAYO_EN_HECES = [
+  'ELISA AMEBAS',
+  'ELISA Giardia',
+  'Azucares Reductores',
+  'Rotavirus',
+  'Adenovirus',
+  'Sangre Oculta',
+  'H. Pylori Ag.',
+  'Verotoxina + E.Coli 0157',
+  'Sudam III',
+  'Clostridium Difficile Tox. A y B',
+  'Criptosporidium'
+]
+
+export const DROGAS_DE_ABUSO = [
+  'Cocaina',
+  'THC',
+  'Benzodiazepinas',
+  'H. Pylori Ag.',
+  'Verotoxina + E.Coli 0157',
+  'Sudam III',
+  'Clostridium Difficile Tox. A y B',
+  'Criptosporidium'
+]
+
+export const PANEL_DE_ALERGIAS = [
+  'Respiratorio',
+  'Alimenticio',
+  'Pediatrico'
+]
+
+export const MEDIO_INTERNO = [
+  'Gasometria',
+  'Osmolaridad Urinaria',
+  'Osmolaridad Plasmatica',
+  'Oximetria'
+]
