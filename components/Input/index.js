3fc519c3e5477cadf230cda52ba56d4a58ea2b77
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-15 20:35:38 -0400

    remove autoFocus BUG

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,21 +1,37 @@
 import { forwardRef } from 'react'
 
-export const Input = forwardRef(({ errors: error, onChange, type, name, label, placeholder }, ref) => {
+export const Input = forwardRef(({
+  errors: error,
+  onChange,
+  type,
+  name,
+  label,
+  placeholder,
+  children
+}, ref) => {
   return (
     <>
-      <input
-        onChange={onChange}
-        name={name}
-        label={label}
-        ref={ref}
-        type={type}
-        placeholder={placeholder}
-        autoFocus
-      />
+      <label>
+        <p> {children} </p>
+        <input
+          onChange={onChange}
+          name={name}
+          label={label}
+          ref={ref}
+          type={type}
+          placeholder={placeholder}
+        />
+      </label>
       <style jsx>{`
+      label {
+        display: flex;
+        justify-content: left;
+        align-items: center;
+        height: 35px;
+        flex-direction: row-reverse;
+      }
       input {
-        display: block;
-        margin: 1rem 0;
+        margin: 1.75rem 0;
         border: ${error ? '1px solid #f00' : 'none'}
        }
     `}
