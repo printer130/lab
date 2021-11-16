c76299e2eb4fc786eabaa74c36685b25249f7162
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-15 20:36:15 -0400

    Add listOfAnalisis component  🔥

---
+++ components/ListOfAnalisys/index.js
@@ -0,0 +1,23 @@
+import { Input } from 'components'
+
+export const ListOfAnalisys = ({
+  listOfAnalisys,
+  onChange,
+  title
+}) => {
+  return (
+    <div>
+      <h3> {title} </h3>
+      {
+      listOfAnalisys.map(word =>
+        <Input
+          name={word}
+          key={word}
+          onChange={onChange}
+          type='checkbox'
+        > {word}
+        </Input>)
+     }
+    </div>
+  )
+}
