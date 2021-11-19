850f700748fe83f168f2dae37d3be3e0936c5633
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-19 17:52:46 -0400

    Label tag idk ⚔️

--- components/Select/index.js
+++ components/Select/index.js
@@ -2,16 +2,21 @@ import { forwardRef } from 'react'
 
 export const Select = forwardRef(({ onChange, name, label }, ref) => (
   <>
-    <label>{label}</label>
-    <div>
-      <select name={name} ref={ref} onChange={onChange}>
-        <option value='laboratorio1'>laboratorio1</option>
-        <option value='laboratorio2'>laboratorio2</option>
-      </select>
-    </div>
+    <label>
+      {label}
+      <div>
+        <select name={name} ref={ref} onChange={onChange}>
+          <option value='laboratorio1'>laboratorio1</option>
+          <option value='laboratorio2'>laboratorio2</option>
+          <option value='laboratorio3'>laboratorio3</option>
+          <option value='laboratorio4'>laboratorio4</option>
+        </select>
+      </div>
+    </label>
     <style jsx>{`
       select {
         width: 100%;
+        padding: .15rem .55rem;
       }  
     `}
     </style>
