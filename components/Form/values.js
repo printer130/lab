f9e171d20ab8fddee3d1e06da54500e8a4c53124
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-19 17:45:33 -0400

    Add doctor input to register client

---
+++ components/Form/values.js
@@ -0,0 +1,63 @@
+export const REGISTER_VALUES = ({ errors }) => [
+  {
+    type: 'number',
+    error: errors?.ci,
+    label: 'Carnet de identidad: ',
+    placeholder: 'C.I',
+    name: 'ci',
+    options: {
+      valueAsNumber: true
+    }
+  },
+  {
+    type: 'text',
+    label: 'Nombre Completo',
+    error: errors?.fullName,
+    placeholder: 'Nombre Completo...',
+    name: 'fullName',
+    options: {}
+  },
+  {
+    type: 'date',
+    label: 'Fecha de nacimiento',
+    placeholder: 'Fecha de Nacimiento',
+    name: 'birth',
+    options: {
+      valueAsDate: true
+    }
+  },
+  {
+    type: 'number',
+    label: 'Número de celular',
+    error: errors?.phone,
+    placeholder: 'Celular',
+    name: 'phone',
+    options: {
+      valueAsNumber: true
+    }
+  },
+  {
+    type: 'number',
+    error: errors?.nit,
+    label: 'N.I.T.',
+    placeholder: 'NIT',
+    name: 'nit',
+    options: {
+      valueAsNumber: true
+    }
+  },
+  {
+    label: 'Razon Social',
+    type: 'text',
+    placeholder: 'Razon Social',
+    name: 'socialReason',
+    options: {}
+  },
+  {
+    label: 'Nombre del médico',
+    type: 'text',
+    placeholder: 'Médico...',
+    name: 'doctor',
+    options: {}
+  }
+]
