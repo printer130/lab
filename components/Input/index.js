f1c6bfb6e4771373e1944c45bbbff9cae475efa4
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-19 17:45:57 -0400

    Add some css to improve input component

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,3 +1,4 @@
+import { ErrorMessage } from 'components'
 import { forwardRef } from 'react'
 
 export const Input = forwardRef(({
@@ -5,6 +6,7 @@ export const Input = forwardRef(({
   onChange,
   type,
   name,
+  checked,
   label,
   placeholder,
   children
@@ -12,27 +14,53 @@ export const Input = forwardRef(({
   return (
     <>
       <label>
-        <p> {children} </p>
+        <strong> {children} </strong>
         <input
           onChange={onChange}
           name={name}
+          checked={checked}
           label={label}
           ref={ref}
           type={type}
           placeholder={placeholder}
         />
+        <ErrorMessage>{error?.message}</ErrorMessage>
       </label>
       <style jsx>{`
       label {
         display: flex;
+        margin: 0 auto;
         justify-content: left;
-        align-items: center;
-        height: 35px;
-        flex-direction: row-reverse;
+        align-items: ${type !== 'checkbox' ? 'start' : 'center'};
+        flex-direction: ${type !== 'checkbox' ? 'column' : 'row-reverse'};
       }
+
+      strong {
+        margin: ${type !== 'checkbox' ? '0' : '0 .75rem'};
+        font-weight: 600;
+        align-items: center;
+      }
+
       input {
-        margin: 1.75rem 0;
-        border: ${error ? '1px solid #f00' : 'none'}
+        flex-direction: ${type !== 'checkbox' ? 'column' : 'row-reverse'};
+        letter-spacing: .75px;
+        align-items: center;
+        height: 33px;
+        max-width: 189px;
+        min-width: 189px;
+        width: 189px;
+        padding: .35rem .75rem;
+        flex-direction: initial;
+        margin: ${type !== 'checkbox' ? '.75rem 0 1.5rem 0' : '.7rem .15rem'};
+        border: ${error ? '1px solid #f00' : '.35px solid #101010'}
+       }
+
+       input[type=number]::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+       }
+
+       input[type=date] {
+        width: -webkit-fill-available;
        }
     `}
       </style>
