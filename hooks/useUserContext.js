7f1b926b581622268a304ae046a3a37bd8422325
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-11-19 17:51:34 -0400

    Add lab context

--- hooks/useUserContext.js
+++ hooks/useUserContext.js
@@ -1,14 +1,24 @@
-import { createContext } from 'react'
+import { USER_STATES } from 'const/user_states'
+import { createContext, useEffect, useState } from 'react'
 import { useUser } from './useUser'
+import { useRouter } from 'next/router'
 
 export const UserContext = createContext()
 
 export function UserProvider ({ children }) {
   const { user } = useUser()
+  const [lab, setLab] = useState()
+  const router = useRouter()
 
   const value = {
-    user
+    user,
+    onLab: setLab,
+    lab
   }
+
+  useEffect(() => {
+    user === USER_STATES.NOT_LOGGED && router.replace('/')
+  }, [user])
 
   return (
     <UserContext.Provider value={value}>
