867c3a4d7d12bfcf13380fa9e532b3de55a8cd98
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2022-01-07 16:22:46 -0400

    🔥🔥🔥🔥

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,14 +1,53 @@
 /* eslint-disable react/jsx-closing-tag-location */
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { signOut, useSession } from 'next-auth/react'
 import { Button } from 'components'
 import Link from 'next/link'
 import { SIGN_OUT } from 'const/config'
 import { Logo } from 'components/Logo'
 import { useRouter } from 'next/router'
+import { motion, useCycle } from 'framer-motion'
+import { MenuToggle } from './MenuToggle'
 
+// Naive implementation - in reality would want to attach
+// a window or resize listener. Also use state/layoutEffect instead of ref/effect
+// if this is important to know on initial client render.
+// It would be safer to  return null for unmeasured states.
+// export const useDimensions = ref => {
+//   const dimensions = useRef({ width: 0, height: 0 })
+
+//   useEffect(() => {
+//     dimensions.current.width = ref.current.offsetWidth
+//     dimensions.current.height = ref.current.offsetHeight
+//   }, [])
+
+//   return dimensions.current
+// }
+
+// const sidebar = {
+//   open: (height = 1000) => ({
+//     clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+//     transition: {
+//       type: 'spring',
+//       stiffness: 20,
+//       restDelta: 2
+//     }
+//   }),
+//   closed: {
+//     clipPath: 'circle(30px at 40px 40px)',
+//     transition: {
+//       delay: 0.5,
+//       type: 'spring',
+//       stiffness: 400,
+//       damping: 40
+//     }
+//   }
+// }
 export const Nav = () => {
   // const { user } = useUser()
+  // const [isOpen, toggleOpen] = useCycle(false, true)
+  // const containerRef = useRef(null)
+  // const { height } = useDimensions(containerRef)
   const [checked, setChecked] = useState(false)
   const session = useSession()
   const email = session?.data?.token?.email
@@ -18,8 +57,7 @@ export const Nav = () => {
     signOut({
       redirect: false,
       callbackUrl: `${SIGN_OUT}`
-    })
-      .then(data => push(data.url))
+    }).then(data => push(data.url))
   }
   const handleClic = () => {
     setChecked(!checked)
@@ -35,55 +73,34 @@ export const Nav = () => {
         className='menu-checkbox'
       />
       <header className='header'>
-        <Logo
-          white='#fff'
-          blue='#1A90C0'
-          innerB='#252525'
-          a='#fff'
-        />
-        {
-          email &&
-            <>
-              <small>
-                {email}
-              </small>
-              <div className='header-content'>
-                <label
-                  htmlFor='open'
-                  className='menu-open'
-                />
-                <nav className='menu'>
-                  <Link href='/arqueo'>
-                    <a onClick={handleClic}>
-                      Arqueo
-                    </a>
-                  </Link>
-                  <Link href='/registro'>
-                    <a onClick={handleClic}>
-                      Registro
-                    </a>
-                  </Link>
-                  <Link href='/ordenes'>
-                    <a onClick={handleClic}>
-                      Lista de Ordenes
-                    </a>
-                  </Link>
-                  <Link href='/reactivos'>
-                    <a onClick={handleClic}>
-                      Reactivos
-                    </a>
-                  </Link>
-                  <Button
-                    onChange={handleButton}
-                  >
-                    Cerrar Session
-                  </Button>
-                </nav>
-              </div>
-            </>
-          }
+        <Logo white='#fff' blue='#1A90C0' innerB='#252525' a='#fff' />
+        {email && (
+          <>
+            <small>{email}</small>
+
+            <div className='header-content'>
+              <label htmlFor='open' className='menu-open' />
+              <nav className='menu'>
+                <Link href='/arqueo'>
+                  <a onClick={handleClic}>Arqueo</a>
+                </Link>
+                <Link href='/registro'>
+                  <a onClick={handleClic}>Registro</a>
+                </Link>
+                <Link href='/ordenes'>
+                  <a onClick={handleClic}>Lista de Ordenes</a>
+                </Link>
+                <Link href='/reactivos'>
+                  <a onClick={handleClic}>Reactivos</a>
+                </Link>
+                <Button onChange={handleButton}>Cerrar Session</Button>
+              </nav>
+            </div>
+          </>
+        )}
       </header>
-      <style jsx>{`
+      <style jsx>
+        {`
           input {
             display: none;
           }
@@ -106,8 +123,8 @@ export const Nav = () => {
           }
           a {
             color: #fff;
-            outline: .2px solid #eee;
-            padding: .25rem .5rem ;
+            outline: 0.2px solid #eee;
+            padding: 0.25rem 0.5rem;
             margin: 1rem 0;
             display: flex;
             flex-direction: row;
@@ -137,7 +154,7 @@ export const Nav = () => {
             display: flex;
             flex-direction: column;
             padding: 16px;
-            letter-spacing: .6px;
+            letter-spacing: 0.6px;
             font-size: 1.6rem;
             visibility: hidden;
             position: absolute;
@@ -178,7 +195,7 @@ export const Nav = () => {
           .menu-checkbox:checked ~ .header .header-content .menu-open {
             background: none;
           }
-          
+
           @media screen and (min-width: 789px) {
             .mnu {
               transform: translateY(0);
--- pages/registro/[ci]/index.js
+++ pages/registro/[ci]/index.js
@@ -114,20 +114,6 @@ export default function RegisterNewCI ({ order = '' }) {
                 {receipt.ownerCi}
               </p>
             </fieldset>
-            <Popover className='relative'>
-              <Popover.Button>Solutions</Popover.Button>
-
-              <Popover.Panel className='absolute z-10'>
-                <div className='grid grid-cols-2'>
-                  <a href='/analytics'>Analytics</a>
-                  <a href='/engagement'>Engagement</a>
-                  <a href='/security'>Security</a>
-                  <a href='/integrations'>Integrations</a>
-                </div>
-
-                <img src='/solutions.jpg' alt='' />
-              </Popover.Panel>
-            </Popover>
           </div>
         )}
         <Link href='/registro'>
