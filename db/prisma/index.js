4525715f382944350d2cbe3aaad27e007d7794e3
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2022-01-06 23:18:51 -0400

    🔥🔥🔥🔥

--- db/prisma/index.js
+++ db/prisma/index.js
@@ -1,13 +1,12 @@
 // @ts-check
 const { PrismaClient } = require('@prisma/client')
 
-let prisma = PrismaClient || undefined
-
-prisma =
+const prisma =
   global.prisma ||
   new PrismaClient({
     log: ['error']
   })
+
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
-exports.default = { prisma }
+module.exports = { prisma }
