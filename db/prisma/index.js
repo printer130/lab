982c29f60421c402feadd8f4ec2c384d5e2c2f0e
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2022-01-27 10:29:06 -0400

    ✅DB

--- db/prisma/index.js
+++ db/prisma/index.js
@@ -1,11 +1,25 @@
-const { PrismaClient } = require('@prisma/client')
+// const { PrismaClient } = require('@prisma/client')
 
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['error']
-  })
+// const prisma =
+//   global.prisma ||
+//   new PrismaClient({
+//     log: ['error']
+//   })
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
-module.exports = { prisma }
+// module.exports = { prisma }
+import { PrismaClient } from '@prisma/client'
+
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export default prisma
