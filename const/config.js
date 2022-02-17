caa419327fb255912e397b017864f972ded1364f
leoci <leonardo@localhost.localdomain>
2022-02-17 09:50:24 -0400

    Redirect :D

--- const/config.js
+++ const/config.js
@@ -1,8 +1,8 @@
 export const URL_CALLBACK =
   process.env.NODE_ENV === 'production'
-    ? 'https://pedro-farmacias.vercel.app/registro'
+    ? 'https://lab-blond.vercel.app/registro'
     : 'http://192.168.0.3:3000/registro'
 export const SIGN_OUT =
   process.env.NODE_ENV === 'production'
-    ? 'https://pedro-farmacias.vercel.app/'
+    ? 'https://lab-blond.vercel.app/'
     : 'http://192.168.0.3:3000/'
