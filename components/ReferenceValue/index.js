6bac5bc6e0b857652c07bda06d9a23aa8d6745f5
leoci <leonardotobe94@gmail.com>
2022-03-18 21:44:26 -0400

    Fix pixel Reference :s

--- components/ReferenceValue/index.js
+++ components/ReferenceValue/index.js
@@ -3,11 +3,11 @@ export function ReferenceValue ({ measurement, reference, brand }) {
   return (
     <div className='reference_value'>
       (<p>{brand} </p>
-      {measurement && <p>- {measurement}</p>}
+      {measurement && <p>{measurement}</p>}
       {reference &&
         <div>
           <p>
-            <strong>- Valor de Referencia: </strong>
+            <strong>Valor de Referencia: </strong>
             {reference}
           </p>
         </div>}
