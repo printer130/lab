ecea7a1345dd4270b24c117258762166d6be827c
leoncii <leonardotobe94@gmail.com>
2022-04-12 20:29:49 -0400

    Doc PDF styles

--- utils/pdf/index.js
+++ utils/pdf/index.js
@@ -2,6 +2,8 @@ import { getAge } from 'hooks/dateTime/getAge'
 import JsPDF from 'jspdf'
 import { toast } from 'react-toastify'
 import { unique } from 'utils/unique'
+const rounded = 3
+const heightBox = 13
 
 export const GeneratePDF = ({ id, data, lab, onLoading, onModal }) => {
   // onLoading(true)
@@ -11,8 +13,10 @@ export const GeneratePDF = ({ id, data, lab, onLoading, onModal }) => {
     unit: 'px',
     format: [612, 793]
   })
+
   const pageWidth = doc.internal.pageSize.getWidth()
   const sex = data.owner.sex === 'male' ? 'Masculino' : 'Femenino'
+
   doc.setFontSize(10)
   doc.html(el, {
     autoPaging: true,
@@ -22,23 +26,68 @@ export const GeneratePDF = ({ id, data, lab, onLoading, onModal }) => {
       const pageCount = doc.internal.getNumberOfPages()
       for (let i = 1; i <= pageCount; i++) {
         doc.setPage(i)
+        // Lines Rectangles
+
+        doc.setDrawColor(0)
+        doc.setFillColor(235, 235, 235)
+
+        // PACIENTE // LEFT
+        // fill
+        doc.roundedRect(162, 22, 46, heightBox, rounded, rounded, 'F')
+        // rectangle
+        doc.roundedRect(210, 22, 250, heightBox, rounded, rounded, 'D')
+        // EDAD
+        // fill
+        doc.roundedRect(162, 45, 31, heightBox, rounded, rounded, 'F')
+        // rectangle
+        doc.roundedRect(196, 45, 22, heightBox, rounded, rounded, 'D')
+        // Genero
+        // fill
+        doc.roundedRect(162, 75, 40, heightBox, rounded, rounded, 'F')
+        // rectangle
+        doc.roundedRect(204, 75, 75, heightBox, rounded, rounded, 'D')
+        // Medico
+        // fill
+        doc.roundedRect(162, 105, 40, heightBox, rounded, rounded, 'F')
+        // rectangle
+        doc.roundedRect(202, 105, 116, heightBox, rounded, rounded, 'D')
+
+        // codigo // RIGHT
+        // fill
+        doc.roundedRect(319, 45, 35, heightBox, rounded, rounded, 'F')
+        // rectangle
+        doc.roundedRect(357, 45, 75, heightBox, rounded, rounded, 'D')
+        // date // RIGHT
+        // fill
+        doc.roundedRect(319, 75, 107, heightBox, rounded, rounded, 'F')
+        // rectangle
+        doc.roundedRect(429, 75, 60, heightBox, rounded, rounded, 'D')
+        // impresion // RIGHT
+        // fill
+        doc.roundedRect(323, 105, 85, heightBox, rounded, rounded, 'F')
+        // rectangle
+        doc.roundedRect(411, 105, 60, heightBox, rounded, rounded, 'D')
+        // LINE SEPARATOR
+        doc.setLineWidth(2)
+        doc.line(0, 150, 615, 150)
+
         // HEADER
         doc.addImage(lab.image, 'JPEG', 0, 0, 150, 0)
         // doc.addImage('/, 'PNG', 0, 0, 150, 0)
-        doc.text(166, 25, 'Paciente: ' + data.owner.fullName)
-        doc.text(166, 55, 'Edad: ' + getAge(data.owner.birth))
-        doc.text(166, 85, 'Género: ' + sex)
+        doc.text(166, 25, 'Paciente:   ' + data.owner.fullName, { baseline: 'hanging' })
+        doc.text(166, 55, 'Edad:   ' + getAge(data.owner.birth))
+        doc.text(166, 85, 'Género:   ' + sex)
         doc.text(166, 115, 'Médico: ' + data.owner.doctor)
-        doc.text(320, 55, 'Código: ' + unique({
+        doc.text(320, 55, 'Código:  ' + unique({
           labName: data.labName,
           ownerCi: data.owner.ci,
           cuiid: data.cuiid,
           id: data.owner.id
         }))
-        doc.text(320, 85, 'Fécha toma de muestra: ' + data.createdAt.slice(0, 10))
-        doc.text(320, 115, 'Fécha de impresión: ' + new Date().toLocaleDateString().split('T')[0])
+        doc.text(320, 85, 'Fécha toma de muestra:  ' + data.createdAt.slice(0, 10))
+        doc.text(320, 115, 'Fécha de impresión:  ' + new Date().toLocaleDateString().split('T')[0])
         // FOOTER
-        doc.text(pageWidth / 2, 720, lab?.address || '', { align: 'center' })
+        doc.text(pageWidth / 2, 720, lab?.address ?? '', { align: 'center' })
         doc.text(271, 740, '72730216 - 68518882', { align: 'center' })
         doc.text(270, 760, 'Cochabamba - Bolivia', { align: 'center' })
         // Page
